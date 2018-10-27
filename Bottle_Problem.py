6f3c9139919aa243a8055272a876870e981d42c2
Coleman <colemanrose3@yahoo.com>
2018-10-27 11:20:51 -0500

    bottle_problem solutions

--- Bottle_Problem.py
+++ Bottle_Problem.py
@@ -1,22 +1,6 @@
 from fractions import gcd
 
 """
-You have two unmarked bottles that can hold 3 liters and 5 liters respectively, and a bathtub with unlimited water.
-You are allowed to either fill a bottle, empty a bottle or pour one bottle into the other until either the source bottle
-is empty or the target bottle is full.
-
-- how can 1 liter be measured?
-- how can 4 liters be measured?
-
-Implement a solution that would solve this problem and find the minimum amount of steps required to measure the desired amount of water (1 and 4 liters).
-Note that the program should find the shortest solution given only the information above, i.e. the volume of the unmarked bottles and the desired target amount.
-
-Provide the source code (including unit tests) and if necessary, a description of how to execute the code.
-"""
-
-#########################################################################################################################################################
-
-"""
 So after attempting OOP solutions involving instances of a bottle class with attributes for volume and storing their
 contained liquid, I realized I was just trying to be too fancy and that simple algorithmic principles could be applied.
 And I've usually found that creating your idea the simplest way you know how and then refining for speed/efficiency after
